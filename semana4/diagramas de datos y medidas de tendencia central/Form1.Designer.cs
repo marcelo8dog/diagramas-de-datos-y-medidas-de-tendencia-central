edff733b23247928e0857102747a2c91cb0386d9
marcelo8dog <marcelo8dog@hotmail.com>
2021-03-15 19:16:41 -0600

    Add files via upload

---
+++ semana4/diagramas de datos y medidas de tendencia central/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace diagramas_de_datos_y_medidas_de_tendencia_central
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series9 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series10 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.indexNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.valueNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.indexNumber,
+            this.valueNumber});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(412, 518);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // indexNumber
+            // 
+            this.indexNumber.HeaderText = "indexNumber";
+            this.indexNumber.MinimumWidth = 6;
+            this.indexNumber.Name = "indexNumber";
+            this.indexNumber.Width = 125;
+            // 
+            // valueNumber
+            // 
+            this.valueNumber.HeaderText = "valueNumber";
+            this.valueNumber.MinimumWidth = 6;
+            this.valueNumber.Name = "valueNumber";
+            this.valueNumber.Width = 125;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(444, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Graficar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(444, 230);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series2";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series3";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series4";
+            series5.ChartArea = "ChartArea1";
+            series5.Legend = "Legend1";
+            series5.Name = "Series5";
+            series6.ChartArea = "ChartArea1";
+            series6.Legend = "Legend1";
+            series6.Name = "Series6";
+            series7.ChartArea = "ChartArea1";
+            series7.Legend = "Legend1";
+            series7.Name = "Series7";
+            series8.ChartArea = "ChartArea1";
+            series8.Legend = "Legend1";
+            series8.Name = "Series8";
+            series9.ChartArea = "ChartArea1";
+            series9.Legend = "Legend1";
+            series9.Name = "Series9";
+            series10.ChartArea = "ChartArea1";
+            series10.Legend = "Legend1";
+            series10.Name = "Series10";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Series.Add(series4);
+            this.chart1.Series.Add(series5);
+            this.chart1.Series.Add(series6);
+            this.chart1.Series.Add(series7);
+            this.chart1.Series.Add(series8);
+            this.chart1.Series.Add(series9);
+            this.chart1.Series.Add(series10);
+            this.chart1.Size = new System.Drawing.Size(682, 300);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(484, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "promedio:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(484, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "varianza:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(484, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "desviación estándar";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(645, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(645, 112);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(645, 155);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1138, 542);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn indexNumber;
+        private System.Windows.Forms.DataGridViewTextBoxColumn valueNumber;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ semana4/diagramas de datos y medidas de tendencia central/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace diagramas_de_datos_y_medidas_de_tendencia_central
+{
+    public partial class Form1 : Form
+    {
+
+        private Random generador = new Random();
+        private int alturaMin = 160;
+        private int alturaMax = 200;
+        private int numeroDeDatos = 100;
+        private double []numeros;
+        private double arriba1, arriba2, arriba3, arriba4;
+        private double abajo1, abajo2, abajo3, abajo4;
+
+        public Form1()
+        {
+            InitializeComponent();
+            inicializarGrafica();
+            generarDatosAleatorios();
+            
+        }
+
+        private void calcularMedidasTendenciaCentral()
+        {
+            double promedioOut = promedio(numeros);
+            double varOut = var(numeros);
+            double stdOut = std(numeros);
+
+            textBox1.Text = Convert.ToString(promedioOut);
+            textBox2.Text = Convert.ToString(varOut);
+            textBox3.Text = Convert.ToString(stdOut);
+
+            arriba1 = promedioOut + 1 * stdOut;
+            graficarFuncionConstante(1,arriba1);
+            arriba2 = promedioOut + 2 * stdOut;
+            graficarFuncionConstante(2, arriba2);
+            arriba3 = promedioOut + 3 * stdOut;
+            graficarFuncionConstante(3, arriba3);
+            arriba4 = promedioOut + 4 * stdOut;
+            graficarFuncionConstante(4, arriba4);
+
+            abajo1 = promedioOut - 1 * stdOut;
+            graficarFuncionConstante(5, abajo1);
+            abajo2 = promedioOut - 2 * stdOut;
+            graficarFuncionConstante(6, abajo2);
+            abajo3 = promedioOut - 3 * stdOut;
+            graficarFuncionConstante(7, abajo3);
+            abajo4 = promedioOut - 4 * stdOut;
+            graficarFuncionConstante(8, abajo4);
+
+            graficarFuncionConstante(9, promedioOut);
+        }
+
+        private void graficarFuncionConstante(int serie, double valorConstante)
+        {
+            chart1.Series[serie].ChartType = SeriesChartType.Line;
+            chart1.Series[serie].IsVisibleInLegend = false;
+            limpiarSerie(serie);
+            for (int i = 0; i < numeroDeDatos -1 ; i++)
+            {
+                chart1.Series[serie].Points.AddY(valorConstante);
+            }
+        }
+
+        private void generarDatosAleatorios()
+        {
+            
+
+            for (int i = 1; i <= numeroDeDatos; i++)
+            {
+                DataGridViewRow fila = new DataGridViewRow();
+                fila.CreateCells(dataGridView1);
+                fila.Cells[0].Value = i;
+                fila.Cells[1].Value = generador.Next(alturaMin, alturaMax);
+
+                dataGridView1.Rows.Add(fila);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //chart1.Series.Clear();
+            //chart1.Series[0].Points.Clear();
+            limpiarSerie(0);
+            //chart1.Series.Add("1");
+            numeros = new double[dataGridView1.Rows.Count];
+            //numeros = null;
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            {
+                chart1.Series[0].Points.AddY(dataGridView1.Rows[i].Cells[1].Value);
+                numeros[i] = Convert.ToDouble(dataGridView1.Rows[i].Cells[1].Value);
+            }
+            calcularMedidasTendenciaCentral();
+
+        }
+
+        private void limpiarSerie(int serie)
+        {
+            chart1.Series[serie].Points.Clear();
+        }
+
+        private void inicializarGrafica()
+        {
+            
+            chart1.ChartAreas[0].AxisX.MajorGrid.LineColor = Color.White;
+            chart1.ChartAreas[0].AxisY.MajorGrid.LineColor = Color.White;
+            //chart1.Series.Add("V");
+            chart1.Series[0].ChartType = SeriesChartType.Line;
+            chart1.Series[0].IsVisibleInLegend = false;
+            //chart1.ChartAreas[0].AxisY.Minimum = alturaMin-20;
+            //chart1.ChartAreas[0].AxisY.Maximum = 5;
+        }
+
+        static double promedio(double[] input)
+        {
+            double output = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                output += input[i];
+            }
+
+            output = output / input.Length;
+
+            return output;
+        }
+
+        static double var(double[] input)
+        {
+            double media = promedio(input);
+            double v = 0;
+            int N = input.Length;
+            for (int i = 0; i < N; i++)
+            {
+                v += Math.Pow(input[i] - media, 2);
+            }
+            v /= (N - 1);
+            return v;
+        }
+
+        static double std(double[] input)
+        {
+            double v = var(input);
+            double s = Math.Sqrt(v);
+
+            return s;
+        }
+
+      
+    }
+}
